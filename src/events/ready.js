2eed3b0cceb93ba71cbc74f2eeedca533894956d
SySLink <shadow.dark2232@gmail.com>
2026-01-03 14:58:16 +0300

    chore: final cleanup and standardization of bot activity

--- src/events/ready.js
+++ src/events/ready.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
 
-        client.user.setActivity('icl ts pmo fr fr u pmo so much amir', { type: ActivityType.Playing });
+        client.user.setActivity('Helping People !', { type: ActivityType.Playing });
 
         if (!config.guildId) {
             console.warn('DISCORD_GUILD_ID is not defined. Guild locking is disabled.');
